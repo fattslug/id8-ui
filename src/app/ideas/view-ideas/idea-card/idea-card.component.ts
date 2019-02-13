61ffc7f666d5a8750bc4ba23031a0171fbad3bf8
Sean Powell <ssppowell@gmail.com>
2019-02-13 00:00:38 -0500

    Adding view idea components, id8 logo directive

---
+++ src/app/ideas/view-ideas/idea-card/idea-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'idea-card',
+  templateUrl: './idea-card.component.html',
+  styleUrls: ['./idea-card.component.scss']
+})
+export class IdeaCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
