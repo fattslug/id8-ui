79fadcdec2ad9a17b040785781f3bccbf72fbc4e
Sean Powell <sean.powell@aviva.com>
2019-03-26 11:07:27 -0400

    Cleaning up and adding like button scaffolding

---
+++ src/app/voting/voting.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VoteButtonComponent } from './vote-button/vote-button.component';
+
+@NgModule({
+  declarations: [VoteButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class VotingModule { }
